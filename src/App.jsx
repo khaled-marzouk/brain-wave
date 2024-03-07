2acb644db3efe190df356f8bf929f6a382b43d20
khaled-marzouk <khaledmarzouk190@gmail.com>
2024-03-07 13:48:08 +0200

    creating services section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./components/Collaboration";
 // import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 function App() {
 	return (
 		<>
@@ -12,6 +13,7 @@ function App() {
 				<Hero />
 				<Benfits />
 				<Collaboration />
+				<Services />
 			</div>
 			<ButtonGradient />
 		</>
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,4 +1,4 @@
-const Heading = ({ className, title }) => {
+const Heading = ({ className, title, text }) => {
 	return (
 		<div
 			className={`${
@@ -6,6 +6,7 @@ const Heading = ({ className, title }) => {
 			} max-w-[50rem] mb-12 lg:mb-20 mx-auto text-center`}
 		>
 			{title && <h2 className="h2">{title}</h2>}
+			{text && <p className="body-2 mt-4 text-n-4">{text}</p>}
 		</div>
 	);
 };
